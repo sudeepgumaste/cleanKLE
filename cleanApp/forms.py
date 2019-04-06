4586518bdb79d2b8856c3bc23d92963e43b4ded6
sudeepgumaste <sudeepgumaste@protonmail.com>
2019-04-06 13:35:54 +0530

    added custom css to login form

--- cleanApp/forms.py
+++ cleanApp/forms.py
@@ -4,8 +4,23 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo,ValidationEr
 
 
 class loginForm(FlaskForm):
-    usn = StringField('usn', validators=[DataRequired()])
+    usn = StringField('USN', validators=[DataRequired()])
 
-    password = PasswordField ('password', validators=[DataRequired()])
+    password = PasswordField ('Password', validators=[DataRequired()])
 
     login = SubmitField ('Login')
+
+
+class registerForm(FlaskForm):
+    username = StringField('User Name', validators=[DataRequired(), Length(min=5, max=10)])
+
+    password = PasswordField('Password', validators = [DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators = [DataRequired(),EqualTo('password')])
+
+    usn = StringField('USN', validators=[DataRequired()])
+
+    branch  = StringField('Branch', validators=[DataRequired()])
+
+    
+
+    submit = SubmitField('Sign Up')
